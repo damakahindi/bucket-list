2b09b14751d3168a4388ca8845fd05c63ff69606
Esther Dama Kahindi <esther.kahindi@andela.com>
2018-02-28 02:32:24 +0300

    Add refinement to the look of th elanding page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './images/other.png';
+import logo from './images/logo.png';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
+        <h1 className="App-title">Dream Share Discover</h1>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Create your bucketlist, share and discover opportunities.
         </p>
       </div>
     );
